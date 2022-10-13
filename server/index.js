2f1f9aac8e7e5e072c4d4ffe3b1e92c494cdfe5a
adparekh <aparekh0110@gmail.com>
2022-10-13 17:20:52 -0400

    Deployment

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
+
 const CONNECTION_URL =
   "mongodb+srv://adparekh:adparekh123@cluster0.qfq58g2.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 4000;
